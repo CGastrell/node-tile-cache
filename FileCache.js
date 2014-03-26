2ac7898606da5769a622bb848dc57509a899ccad
Christian Gastrell <cg@ubuntu-vm.(none)>
2014-03-26 04:09:02 -0300

    ...

--- FileCache.js
+++ FileCache.js
@@ -1,10 +1,10 @@
 var crypto = require('crypto');
 var fs = require('fs');
-var dir = './cache';
-var md5sum = crypto.createHash('md5');
+var utils = require('util');
 
 function Cache(path) {
 	var _dir = path;
+
 	// console.log(_dir + ' exists?: '+fs.existsSync(_dir));
 	if(fs.existsSync(_dir) !== true) {
 		throw new Error("Cache :: can't find path");
@@ -18,24 +18,52 @@ function Cache(path) {
 	console.log('Cache init on ' + fs.realpathSync(_dir));
 
 	function getHash(str) {
-		return md5sum.update(str).digest('hex');
+		return crypto.createHash('md5').update(str).digest('hex');
+	};
+	function name(key) {
+		return _dir + "/" + getHash(key);
+	};
+	function stash(url) {
+
 	};
 
 
 	this.dir = function(newpath) {
 		if(newpath) {
-			this._dir = newpath;
+			_dir = newpath;
 		}
-		return this._dir;
+		console.log(_dir);
+		return _dir;
 	};
+	this.getPath = function(url) {
+		return name(url);
+	}
+	this.get = function(url, expiration) {
+		var cachePath = name(url);
+		expiration = expiration || 30 * 60 * 60 * 1000;
 
-	this.get = function(url) {
-		var hashed = getHash(url);
-		if(fs.existsSync(_dir + '/' + hashed)) {
-			console.log('file exists: cache hit');
-		}else{
-			console.log('file not here, should stash as ' + _dir + '/' + hashed);
+		if(fs.existsSync(cachePath)) {
+			var fileStats = fs.statSync(cachePath);
+			if(new Date() - fileStats.ctime.getTime() > expiration) {
+				//expicho, hay que buscarlo de nuevo
+			}
+			fs.readFile(cachePath, function(err, data){
+				return data;
+			});
 		}
+		// var req = http.request(options, function(res) {
+		// 		console.log('STATUS: ' + res.statusCode);
+		// 		console.log('HEADERS: ' + JSON.stringify(res.headers));
+		// 		res.setEncoding('utf8');
+		// 		res.on('data', function (chunk) {
+		// 		console.log('BODY: ' + chunk);
+		// 	});
+		// });
+
+		console.log(utils.inspect(fileStats));
+		// console.log('file not here, should stash as ' + _dir + '/' + hashed);
+
+		return true;
 	};
 
 	return this;
--- restify-test.js
+++ restify-test.js
@@ -1,5 +1,6 @@
 var restify = require('restify'),
-	filecache = require('./FileCache')('./cache');
+	filecache = require('./FileCache')('./cache'),
+	utils = require('util');
 
 
 function respond(req, res, next) {
@@ -8,23 +9,24 @@ function respond(req, res, next) {
 
 //necesito un tms proxy, que tenga un cache, que tenga un filestorage...
 
-TMS_get = function (req, res, next) {
+TMSProxy = function (req, res, next) {
 	
 	var baseURL = 'http://mapa.ign.gob.ar/geoserver/gwc/service/tms/1.0.0';
 	var xAndFormatSplit = req.params.xAndFormat.split('.');
-	req.params.x = xAndFormatSplit[0];
-	req.params.format = xAndFormatSplit[1];
-	req.params.capa += '@EPSG:3857@png8';
+	var x = xAndFormatSplit[0];
+	var format = xAndFormatSplit[1];
+	var capa = req.params.capa + '@EPSG:3857@png8';
 	delete req.params.xAndFormat;
-	// console.log('fc: '+filecache);
+	// console.log(req);
+	var tileURL = utils.format("%s/%s/%s/%s/%s.%s", baseURL, capa, req.params.z, x, req.params.y, format + "8");
 	// res.send(this);
-	res.send({cacheDir: filecache.dir(),someHash: filecache.get('http://google.com')});
+	res.send({cacheDir: filecache.getPath(tileURL), url: tileURL, some: filecache.get(tileURL), time: new Date()});
 };
 
 var server = restify.createServer();
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
-server.get('/tms/:capa/:z/:y/:xAndFormat', TMS_get);
+server.get('/tms/:capa/:z/:y/:xAndFormat', TMSProxy);
 //$app->get('/tms/:capa/:z/:y/:x\.:format/status.json', 'Controllers\TMS::status');
 
 server.listen(8080, function() {
