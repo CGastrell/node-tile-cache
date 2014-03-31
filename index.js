632f66dac738d8655bc39850c477aee233ed64d9
Christian Gastrell <cg@ubuntu-vm.(none)>
2014-03-31 13:09:23 -0300

    preparando para heroku test

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
-var static = require('node-static');
+var jaws = require("jaws");
+var utils = require("util");
+var fc = require("./FileCache");
+var app = jaws();
 
-var fileServer = new static.Server('./cache');
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-    	console.log(request.url);
-        fileServer.serve(request, response, function (err, result) {
-        	if(err) {
-        		console.log(err);
-        		response.writeHead(err.status, err.headers);
-        		response.end();
-        	}
-        });
-    }).resume();
-}).listen(8080);
+TMSProxy = function (req, res) {
+	var vars = req.route.params;
+	var baseURL = 'http://mapa.ign.gob.ar/geoserver/gwc/service/tms/1.0.0';
+	var capa = vars.capa + '@EPSG:3857@png8';
+	// console.log(req);
+	var tileURL = utils.format("%s/%s/%s/%s/%s.%s", baseURL, capa, vars.z, vars.x, vars.y, vars.format + "8");
+	var cache = new fc();
+	app.addHeader("Content-Type", "image/png");
+	res.statusCode = 200;
+	cache.on('CACHE_MISS',function MISS(){
+		console.log('CACHE MISS:');
+		console.log(arguments);
+	});
+	cache.on('CACHE_HIT',function HIT(){
+		console.log('CACHE HIT:');
+		console.log(arguments);
+	});
+	cache.on('data',function(chunk){
+		console.log('receiving data: '+chunk.length);
+		res.write(chunk);
+	});
+	cache.on('end', function() {
+		res.end();
+	});
+	cache.get(tileURL);
+};
+
+
+app.route("/tms/:capa/:z/:y/:x.:format", TMSProxy);
+
+app.httpServer.listen(8080, function () {
+  console.log("Running now.")
+});
+
+setInterval(1000 * 60 * 60, function () {
+  app.flush();
+});
