dcf979818991ad2990c3baee02b9208c2680521e
Christian Gastrell <cg@ubuntu-vm.(none)>
2014-04-02 04:41:53 -0300

    correccion de x y

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ TMSProxy = function (req, res) {
 	var baseURL = 'http://mapa.ign.gob.ar/geoserver/gwc/service/tms/1.0.0';
 	var capa = vars.capa + '@EPSG:3857@png8';
 	// console.log(req);
-	var tileURL = utils.format("%s/%s/%s/%s/%s.%s", baseURL, capa, vars.z, vars.x, vars.y, vars.format + "8");
+	var tileURL = utils.format("%s/%s/%s/%s/%s.%s", baseURL, capa, vars.z, vars.y, vars.x, vars.format + "8");
 	var cache = new fc();
 	app.addHeader("Content-Type", "image/png");
 	res.statusCode = 200;
